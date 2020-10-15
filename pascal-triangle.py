85965db17524ed5be1c058ed66558d5d5f657f2c
nickrally <nickrally2009@gmail.com>
2020-10-15 11:46:56 -0600

    renamed args, removed commented block

--- pascal-triangle.py
+++ pascal-triangle.py
@@ -1,6 +1,6 @@
-def pascal(num):
+def pascal(number):
     rows = []
-    for n in range(num):
+    for n in range(number):
         rows.append([])
         for i in range(n+1):
             if i == 0 or i == n:
@@ -10,17 +10,9 @@ def pascal(num):
     return rows
 
 
-
-# def pascal(num):
-#     row = []
-#     for n in range(num):
-#         row = [1 if i == 0 or i == n else row[i-1] + row[i] for i in range(n+1)]
-#         yield row
-
-
-def draw(f, num):
-   for row in f(num):
-       print (' '.join(map(str, row)).center(num*2)+'\n')
+def draw(f, number):
+   for row in f(number):
+       print (' '.join(map(str, row)).center(number*2)+'\n')
 
 
 num = 5
